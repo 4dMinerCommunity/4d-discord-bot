7b968fcf4454caba1e46e3cf954132952bbf3525
Redjard <github@julianahl.de>
2023-01-17 23:53:08 +0100

    Initial commit after rewrite from node.js

---
+++ settings.py
@@ -0,0 +1,12 @@
+
+bot_api_key = '<TOKEN>'
+
+server_ids = [954156776671043625]
+
+suggestions_channel = 956667255846993950
+reports_channel = 973645451347767346
+
+suggestions_default_emoji = ['4DUpvote', '4DDownvote']
+poll_default_emoji = ['4DUpvote', '4DDownvote']
+
+debug = False
