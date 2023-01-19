e85d7bb909f000c6ec0992cfcaf8bc0336c8441a
Redjard <github@julianahl.de>
2023-01-19 21:12:42 +0100

    add   max_threadname_length setting

--- settings.py
+++ settings.py
@@ -9,4 +9,6 @@ reports_channel = 973645451347767346
 suggestions_default_emoji = ['4DUpvote', '4DDownvote']
 poll_default_emoji = ['4DUpvote', '4DDownvote']
 
+max_threadname_length = 100  # 100 is discord limit
+
 debug = False
