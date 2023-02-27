c2d053c71a8f8295b79aebe29c98c773ee44b65a
Redjard <github@julianahl.de>
2023-02-27 03:47:33 +0100

    settings for welcome messages

--- settings.py
+++ settings.py
@@ -5,9 +5,16 @@ server_ids = [954156776671043625]
 
 suggestions_channel = 956667255846993950
 reports_channel = 973645451347767346
+welcome_channel = 955240591875276820
 
 suggestions_default_emoji = ['4DUpvote', '4DDownvote']
 poll_default_emoji = ['4DUpvote', '4DDownvote']
+welcome_messages = [
+  "Hey **{}**, welcome to **4D Miner**!",
+  "Hey **{}**, welcome to **4D Miner**!",
+  "Hey **{}**, welcome to **4D Miner**!",
+  "Hey **{}**, welcome to the best 4D Discord Server!",
+]
 
 max_threadname_length = 100  # 100 is discord limit
 
