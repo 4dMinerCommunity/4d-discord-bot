3c88eefbc9638ef21b6b4b9519f1be780d404959
Redjard <github@julianahl.de>
2023-05-09 09:34:27 +0200

    adapt welcome messages to new name system

--- settings.py
+++ settings.py
@@ -10,10 +10,10 @@ welcome_channel = 955240591875276820
 suggestions_default_emoji = ['4DUpvote', '4DDownvote']
 poll_default_emoji = ['4DUpvote', '4DDownvote']
 welcome_messages = [
-  "Hey **{}**, welcome to **4D Miner**!",
-  "Hey **{}**, welcome to **4D Miner**!",
-  "Hey **{}**, welcome to **4D Miner**!",
-  "Hey **{}**, welcome to the best 4D Discord Server!",
+  "Hey {}, welcome to **4D Miner**!",
+  "Hey {}, welcome to **4D Miner**!",
+  "Hey {}, welcome to **4D Miner**!",
+  "Hey {}, welcome to the best 4D Discord Server!",
 ]
 
 suggestions_info_msg_tagname = 'suggestions info message'
